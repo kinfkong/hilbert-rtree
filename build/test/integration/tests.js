4799f8f1ffa37f0ad6233fcda2f915bb1b6bf7d6
Jinggang Wang <kinfkong@126.com>
2023-03-17 16:57:51 +0800

    fixes:

---
+++ build/test/integration/tests.js
@@ -0,0 +1,58 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test = require("tape");
+const index_js_1 = require("../../index.js");
+test("It should be possible to create an RTree by inserting nodes successively", async (t) => {
+    const tree = new index_js_1.RTree({ maxChildrenPerNode: 2 });
+    tree.insert({
+        x: 0,
+        y: 0,
+        data: "Contained point"
+    });
+    tree.insert({
+        x: 1,
+        y: 1,
+        width: 1,
+        height: 1,
+        data: "Contained area"
+    });
+    tree.insert({
+        x: 2,
+        y: 2,
+        width: 1,
+        height: 1,
+        data: "Overlapped area"
+    });
+    t.deepEquals(tree.search({
+        x: 0,
+        y: 0,
+        width: 2,
+        height: 2
+    }), ["Contained point", "Contained area", "Overlapped area"], "Expect query to return overlapped data entries");
+});
+test("It should be possible to create an RTree by batch inserting nodes", async (t) => {
+    const tree = new index_js_1.RTree({ maxChildrenPerNode: 2 });
+    tree.batchInsert([{
+            x: 0,
+            y: 0,
+            data: "Contained point"
+        }, {
+            x: 1,
+            y: 1,
+            width: 1,
+            height: 1,
+            data: "Contained area"
+        }, {
+            x: 2,
+            y: 2,
+            width: 1,
+            height: 1,
+            data: "Overlapped area"
+        }]);
+    t.deepEquals(tree.search({
+        x: 0,
+        y: 0,
+        width: 2,
+        height: 2
+    }), ["Contained point", "Contained area", "Overlapped area"], "Expect query to return overlapped data entries");
+});
